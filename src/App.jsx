38a6d1bc0a86211702d50330984486e6ea3602e1
krishna singh <0.krishna1120@gmail.com>
2026-01-08 20:08:17 +0530

    feat: update App component to include Layout in Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Tasks from './pages/Tasks'
 import Goals from './pages/Goals'
 import Dashboard from './pages/Dashboard'
 import Habits from './pages/Habits'
+import Layout from './layout/layout'
 import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
     <div >
   
       
-      <Routes>
+      <Routes element={<Layout/>}>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/tasks' element={<Tasks/>}/>
         <Route path='/goals' element={<Goals/>}/>
